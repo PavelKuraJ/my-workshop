4488b6b5324a7bbc171f47f462d735284695666c
Pavel Kuryzhov <pavelsqu@gmail.com>
2020-10-14 10:26:46 +0300

    first steps

---
+++ server/index.js
@@ -0,0 +1,33 @@
+// server part
+const fs = require('fs');
+const http = require ('http'); 
+const debug = require('debug')('http')
+
+const server = http.createServer((req, res) => {
+    debug('requested', req.url);
+
+    const path = `./public${req.url === '/' ? '/index.html' : req.url}`;
+
+    // to outside interference checking
+    const ip = res.socket.remoteAddress;
+    const port = res.socket.remotePort;
+    // console.log -> debug
+    debug(`Your IP address is ${ip} and your source port is ${port}.`);
+
+    file = fs.readFile(path, (err, file) =>{
+        if (err){
+            debug('file read error', path, err);
+            res.write('error');
+            res.end();
+            return; 
+        }
+        debug('file read', path);
+        
+        res.write(file);
+        res.end();
+    });   
+    
+    debug('after read file')
+});
+
+server.listen(3000);
